7b9d5b29dd3a137f2c3c0e3bbe68a349d897792d
Wales254 <sydneynyongesa2@gmail.com>
2023-11-17 03:03:26 -0800

    update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print chars or strings.
+ * @str: String pointer.
+ */
+void _puts(char *str)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
+
